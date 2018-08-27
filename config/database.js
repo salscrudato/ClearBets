ac4d03dfd08e754c83fc01283c2fcc6574890442
Salvatore Scrudato <salscrudato@Salvatores-Air.home>
2018-08-27 12:35:07 -0400

    new db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	database: 'mongodb://mlbets:mlbets123@ds261479.mlab.com:61479/mlbets',
+	database: 'mongodb://actionplus31:mdsj0818@ds235132-a0.mlab.com:35132,ds235132-a1.mlab.com:35132/actionplus31?replicaSet=rs-ds235132',
+	//database: 'mongodb://localhost:27017/meanauth',
 	secret: 'yoursecret'
 }
