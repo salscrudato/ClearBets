5f80edea1126e75b684ab3db917fb4426c76045e
Salvatore Scrudato <salscrudato@Salvatores-Air.home>
2018-08-31 02:59:47 -0400

    new db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-	database: 'mongodb://slateadmin:admin123@ds137812.mlab.com:37812/slatechamp',
+	database: 'mongodb://admin:admin123@ds141252-a0.mlab.com:41252,ds141252-a1.mlab.com:41252/slatechamp?replicaSet=rs-ds141252',
 	//database: 'mongodb://localhost:27017/meanauth',
 	secret: 'yoursecret'
 }
